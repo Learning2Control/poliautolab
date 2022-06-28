917a84a211ba9c247e522150da2a01bd78f96490
Giulio Vaccari <giulio09@marcov.it>
2022-06-28 13:36:09 +0200

    First commit

---
+++ packages/localization/src/scripts/getmap_server.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+
+import numpy as np
+import cv2
+
+from localization.srv import GetMap
+import rospy
+from rospy.numpy_msg import numpy_msg
+from sensor_msgs.msg import CompressedImage
+from scipy.interpolate import UnivariateSpline
+# from localization.msg import Floats
+
+def sort_xy(x, y, return_origin=False):
+    """
+    Sort by angle
+
+    :param return_origin: If true returns also the computed origin
+    """
+
+    x0 = np.mean(x)
+    y0 = np.mean(y)
+
+    r = np.sqrt((x-x0)**2 + (y-y0)**2)
+
+    angles = np.where((y-y0) > 0, np.arccos((x-x0)/r), 2*np.pi-np.arccos((x-x0)/r))
+
+    mask = np.argsort(angles)
+
+    x_sorted = x[mask]
+    y_sorted = y[mask]
+
+    if return_origin:
+        return x_sorted, y_sorted, x0, y0
+
+    return x_sorted, y_sorted
+
+def get_angles(x, y, x0=None, y0=None):
+    """
+    Get the angles of the trajectory.
+    
+    :param x: x coordinates
+    :param y: y coordinates
+    :param x0: x coordinate of the origin
+    :param y0: y coordinate of the origin
+    """
+
+    if x0 is None:
+        x0 = np.mean(x)
+    if y0 is None:
+        y0 = np.mean(y)
+
+    r = np.sqrt((x-x0)**2 + (y-y0)**2)
+
+    angles = np.where((y-y0) > 0, np.arccos((x-x0)/r), 2*np.pi-np.arccos((x-x0)/r))
+
+    return angles
+
+def get_interpolation(img, no_preprocessing=False, return_origin=False, scaled=False, method="distance"):
+    """
+    Get the interpolation function of the trajectory of the agent in the environment.
+
+    :param no_preprocessing: if True, the trajectory is not preprocessed
+    :param return_origin: if True, the origin is returned
+    :param scaled: if True, the coordinates are scaled
+    :param method: if "angle", the angles are used, if "distance", the distance from starting point is used
+
+    :return: np.array
+    """
+    top_view = img
+
+    img_hsv = cv2.cvtColor(top_view, cv2.COLOR_RGB2HSV)
+    gray = cv2.cvtColor(top_view, cv2.COLOR_RGB2GRAY)
+
+    lower_yellow = np.array([20,100,150])
+    upper_yellow = np.array([30,255,255])
+
+    mask_yellow = cv2.inRange(img_hsv, lower_yellow, upper_yellow)
+    mask = cv2.bitwise_and(gray, mask_yellow)
+
+    if not no_preprocessing:
+        kernel = np.ones((4, 4), np.uint8)
+        eroded = cv2.erode(mask, kernel) 
+
+        low_threshold = 89
+        high_threshold = 80
+        edges = cv2.Canny(eroded, low_threshold, high_threshold)
+
+        rho = 1  # distance resolution in pixels of the Hough grid
+        theta = np.pi / 180  # angular resolution in radians of the Hough grid
+        threshold = 3  # minimum number of votes (intersections in Hough grid cell)
+        min_line_length = 10  # minimum number of pixels making up a line
+        max_line_gap = 50  # maximum gap in pixels between connectable line segments
+
+        lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
+
+        _points = lines.reshape(-1, 2)
+
+        x, y = _points.T
+
+    else:
+        x_all, y_all = np.nonzero(mask)
+        x, y = x_all, y_all
+
+    x_sorted, y_sorted, x0, y0 = sort_xy(x, y, return_origin=True)
+
+
+    if method == "angle":
+        # Interpolation angle-based
+        angles = get_angles(x_sorted, y_sorted, x0=x0, y0=y0)
+        # Add first and last point
+        spline_input = np.concatenate([[0], angles, [2*np.pi]])
+        x_sorted = np.concatenate([[x_sorted[-1]], x_sorted, [x_sorted[0]]])
+        y_sorted = np.concatenate([[y_sorted[-1]], y_sorted, [y_sorted[0]]])
+    elif method == "distance":
+        # Interpolation distance-based
+        points = np.array([x_sorted, y_sorted]).T
+        distance = np.cumsum( np.sqrt(np.sum( np.diff(points, axis=0)**2, axis=1 )) )
+        spline_input = np.insert(distance, 0, 0)/distance[-1]
+    else:
+        raise ValueError("Unknown method, must be 'angle' or 'distance'")
+
+    s = 0.006 if scaled else 0.01
+
+    spline_x = UnivariateSpline(spline_input, x_sorted, k=2, s=s)
+    spline_y = UnivariateSpline(spline_input, y_sorted, k=2, s=s)
+
+    if return_origin:
+        return spline_x, spline_y, x_sorted, y_sorted, x0, y0
+
+    return [spline_x, spline_y]
+
+
+def handle_add_two_ints(req):
+    ros_data = rospy.wait_for_message("/watchtower00/camera_node/image/compressed", CompressedImage)
+    np_arr = np.frombuffer(ros_data.data, 'u1')
+    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    res = get_interpolation(img, no_preprocessing=False, method="distance")
+    return GetMap(res)
+
+def add_two_ints_server():
+    rospy.init_node('get_map_server')
+    s = rospy.Service('get_map', GetMap, handle_add_two_ints)
+    print("Ready to send a map.")
+    rospy.spin()
+
+if __name__ == "__main__":
+    add_two_ints_server()
---
+++ packages/localization/src/scripts/watcher.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+"""Detects duckiebots from the watchtower using their unique colors.
+
+Publish on watchtower00/localization a PointStamped with the car coordinates.
+
+Edited version of the original example by Simon Halle.
+"""
+__author__ =  'Giulio Vaccari <giulio.vaccari at mail.polimi.it>'
+__version__=  '0.1'
+__license__ = 'MIT'
+# Python libs
+import sys, time
+
+# numpy and scipy
+import numpy as np
+from scipy.ndimage import filters
+
+# OpenCV
+import cv2
+
+# Ros libraries
+import roslib
+import rospy
+
+# Ros Messages
+from sensor_msgs.msg import CompressedImage
+from geometry_msgs.msg import PointStamped
+
+from localization.msg import DuckPose
+
+VERBOSE=True
+
+def get_cars(img):
+    """
+    Returns front and left coord
+    """
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    hsv_color1_blue = np.array([50, 200, 50])
+    hsv_color2_blue = np.array([100, 300, 300])
+    mask_blue = cv2.inRange(img_hsv, hsv_color1_blue, hsv_color2_blue)
+
+    hsv_color1_pink = np.array([100, 50, 200])
+    hsv_color2_pink = np.array([200, 200, 250])
+    mask_pink = cv2.inRange(img_hsv, hsv_color1_pink, hsv_color2_pink)
+    
+    front_coo = np.argwhere(mask_pink==255).mean(axis=0)[::-1]
+    back_coo = np.argwhere(mask_blue==255).mean(axis=0)[::-1]
+    
+    x_center = (front_coo[0] + back_coo[0])/2
+    y_center = (front_coo[1] + back_coo[1])/2
+    angle = np.arctan2(-front_coo[1]+back_coo[1], -front_coo[0]+back_coo[0])
+    
+    return x_center, y_center, angle
+
+class image_feature:
+
+    def __init__(self):
+        '''Initialize ros subscriber'''
+
+        self.coordinates_pub = rospy.Publisher("/watchtower00/localization", PointStamped)
+
+        # subscribed Topic
+        self.subscriber = rospy.Subscriber("/watchtower00/camera_node/image/compressed",
+            CompressedImage, self.callback,  queue_size = 1)
+        if VERBOSE :
+            print("subscribed to /camera/image/compressed")
+
+
+    def callback(self, ros_data):
+        '''Callback function of subscribed topic. 
+        Here images get converted and features detected'''
+        if VERBOSE :
+            print(f'received image of type: "{ros_data.format}"' )
+
+        #### direct conversion to CV2 ####
+        np_arr = np.frombuffer(ros_data.data, 'u1')
+        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+        localized = False
+
+        try:
+            x, y, t = get_cars(image_np)
+            if VERBOSE:
+                print(x, y, t)
+                cv2.circle(image_np, [int(x),int(y)], 20, [0,0,255], -1)
+                cv2.arrowedLine(image_np, [int(x),int(y)], [int(x-50*np.cos(t)),int(y-50*np.sin(t))], [0,255,0], 5)
+            localized = True
+        except ValueError:
+            print("No lines found.")
+
+        cv2.imshow('cv_img', image_np)
+        cv2.waitKey(2)
+
+        pose = DuckPose()
+        pose.header.stamp = rospy.Time.now()
+        pose.header.frame_id = "/watchtower00/localization"
+        if localized:
+            pose.x = x
+            pose.y = y
+            pose.t = t
+            pose.success = True
+        else:
+            pose.x = -1
+            pose.y = -1
+            pose.t = -1
+            pose.success = False
+
+        self.coordinates_pub.publish(pose)
+
+
+def main(args):
+    '''Initializes and cleanup ros node'''
+    ic = image_feature()
+    rospy.init_node('image_feature', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down localization")
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
